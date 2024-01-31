9af11149825ec28debae86801addeb9f8f51c707
kchen53 <kellychen@ufl.edu>
2024-01-31 15:50:53 -0500

    Navbar completed with routing and web background color is set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './components/pages/Home'
-import Game from './components/pages/Game'
-import About from './components/pages/About'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='app'>
-      <Router>
-        <Header />
-        <div className = "App">
-        <Routes>
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path="/game" element={<Game/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-        </Routes>
-        </div>
-      </Router>
-
+      <Header/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,57 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom'
+import {Navbar, Nav, Button, Container} from 'react-bootstrap'
+
+import Home from './pages/Home'
+import Game from './pages/Game'
+import About from './pages/About'
 
 const Header = () => {
+
+  const navBarBackColor = {
+    backgroundColor: '#0052CC',
+  };
+
+  const compButtonStyle = {
+    backgroundColor: '#FF6800',
+    marginRight: '10px',
+  };
+
+  const navBarTextColor = {
+    color: '#ffffff',
+    marginLeft: '10px',
+  };
+
   return (
+  <>
+    <Router> 
+
+    {/* Navigation bar */}
     <div>
-        <Link to="/home" className = "link">Home</Link>
-        <Link to="/game" className = "link">Game</Link>
-        <Link to="/about" className = "link">About</Link>
+      <Navbar expand="lg" style={navBarBackColor}>
+        <Navbar.Brand as={Link} to="/home" style={navBarTextColor}>Escape the Senior Project</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav className="ml-auto">
+                <Button as={Link} to="/home" style={compButtonStyle}>Home</Button>
+                <Button as={Link} to="/game" style={compButtonStyle}>Game</Button>
+                <Button as={Link} to="/about" style={compButtonStyle}>About</Button>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
     </div>
+
+    {/* Routes to components */}
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/game" element={<Game/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
+    </div>
+  
+    </Router>
+  </>
   )
 }
 
