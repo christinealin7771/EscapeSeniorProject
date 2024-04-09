346591be67b0e85eda506a216ac20159a7bd76ff
Radoslav Savenkov <rado9786@gmail.com>
2024-04-09 16:31:25 -0400

    fixing zoom + wire appearance + initial fusebox script commit

--- unityEscapeRoom/Assets/CaesarBookScript.cs
+++ unityEscapeRoom/Assets/CaesarBookScript.cs
@@ -48,7 +48,7 @@ public class CaesarBookScript : MonoBehaviour
 
     private void OnMouseDown() 
     {
-        if(!inFront && canInteract)
+        if(!inFront && canInteract && moving2 == false)
         {
             Debug.Log("Clicked1!");
             if(moving == false){
