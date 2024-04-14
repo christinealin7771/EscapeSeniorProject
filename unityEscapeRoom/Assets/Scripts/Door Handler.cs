17362efa1e1bfb525f8a6c27e6712df03bd2c6ef
Radoslav Savenkov <rado9786@gmail.com>
2024-04-14 07:18:21 -0400

    room 4 randomization, backdoor script, and helper script, fixed room 4 hints, keypadzoom debug fix,

--- unityEscapeRoom/Assets/Scripts/Door Handler.cs
+++ unityEscapeRoom/Assets/Scripts/Door Handler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,7 @@ public class DoorHandler : MonoBehaviour
     public bool doorLocked;
     public string[] roomNameList;
     private Renderer _renderer;
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,12 @@ public class DoorHandler : MonoBehaviour
         if (doorLocked == false) {
             Debug.Log(doorLocked);
             Debug.Log("Clicked on an unlocked door!");
-
+            
+            if(CurrentRoom.roomBuildList[CurrentRoom.counter] != SceneManager.GetActiveScene().buildIndex){
+                CurrentRoom.roomBuildList[CurrentRoom.counter] = SceneManager.GetActiveScene().buildIndex;
+                CurrentRoom.counter++;
+            }
+            
             int randomRoom = Random.Range(0, roomNameList.Length);  // creates a number between 0 and 2
             SceneManager.LoadScene(roomNameList[randomRoom]);   // loads one of the three possible rooms
         }
