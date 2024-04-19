59029c7381dd4a105a6e8f215ec3293240af740a
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2024-04-19 12:12:29 -0400

    Added trigger for service to not be asleep

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Routing from './components/Routes';
 
 function App() {
+  // Function to make the API request to keep deployment alive
+  const makeRequest = async () => {
+      try {
+          const response = await axios.get(process.env.REACT_APP_TRIGGER_URL_DEPLOYMENT);
+          console.log('Response:');
+      } catch (error) {
+          console.error('Error making the request:', error);
+      }
+  };
+
+
+  useEffect(() => {
+    // Make the initial request when the component mounts
+    makeRequest();
+
+    // Set interval to make the request every 14 minutes (14 * 60 * 1000 milliseconds)
+    const intervalId = setInterval(makeRequest, 14 * 60 * 1000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+  // // Set interval to make the request every 14 minutes (14 * 60 * 1000 milliseconds)
+  // setInterval(makeRequest, 14 * 60 * 1000);
+
   return (
     <div className='app'>
       <Router>
