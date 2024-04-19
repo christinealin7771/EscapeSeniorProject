27af6580fcadcfbeb827fe50dc453cc6ce3ee371
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2024-04-19 14:43:51 -0400

    Added useEffect dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
